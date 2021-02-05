585db0cfa18cc76b296e2f10321a74a612d9d879
Chely Ho <adept_inept@yahoo.com>
2021-02-05 04:12:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 //
-// vertices Array of {
+// vertices Array of { name: (x,y), distance: number, predecessor: string }
 // edges Array of [(x,y), (x,y)]
 function bfs(rootNode, vertices, edges){
   const queue = [];
